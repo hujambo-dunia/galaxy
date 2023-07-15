0c51e778d64fe8a5f9a4d263e4a44d6abd233628
hujambo-dunia <michelle.savage.1@gmail.com>
2023-07-15 15:59:08 +1000

    New component file(s)

---
+++ client/src/component-library/GTableLite.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { BTableLite } from "bootstrap-vue";
+</script>
+
+<template>
+    <BTableLite v-bind="$attrs" v-on="$listeners">
+        <slot></slot>
+    </BTableLite>
+</template>
