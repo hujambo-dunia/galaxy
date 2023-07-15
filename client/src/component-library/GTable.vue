c13e0defe472355b643b7a32611a7d4482ecd1c6
hujambo-dunia <michelle.savage.1@gmail.com>
2023-07-15 15:10:38 +1000

    Initial commit; new component file

---
+++ client/src/component-library/GTable.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { BTable } from "bootstrap-vue";
+</script>
+
+<template>
+    <BTable v-bind="$attrs" v-on="$listeners">
+        <slot></slot>
+    </BTable>
+</template>
